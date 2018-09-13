91adf6c663e778608163e9059c1c8538e83d1df7
pedro.tresgallo <pedro.tresgallo@916-IAM-B-HAMER>
2018-09-13 17:57:51 -0500

    Add HelloPoint2

--- pt-GAP-Week1/pt-DrawPointV1/HelloPoint1.js
+++ pt-GAP-Week1/pt-DrawPointV1/HelloPoint1.js
@@ -1,16 +1,16 @@
 ﻿// HelloPoint1.js
 // Vertex shader program
 var VSHADER_SOURCE =
-    'void main() {\n' +
-    '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Coordinates
-    '  gl_PointSize = 10.0;\n' +             // Set the point size
-    '}\n';
+  'void main() {\n' +
+  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Coordinates
+  '  gl_PointSize = 10.0;\n' +             // Set the point size
+  '}\n';
 
 // Fragment shader program
 var FSHADER_SOURCE =
-    'void main() {\n' +
-    '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the color
-    '}\n';
+  'void main() {\n' +
+  '  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n' + // Set the color
+  '}\n';
 
 function main() {
     // Retrieve <canvas> element
@@ -24,13 +24,13 @@ function main() {
     }
 
     // Initialize shaders
-    if (!initShaders(gl, VSHADER + SOURCE, FSHADER + SOURCE)) {
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.log('Failed to initialize shaders.');
         return;
     }
 
     // Set the color for cleaing <canvas>
-    go.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
