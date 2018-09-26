07d34e338715cc3ddfb2635e26e7d9006029c944
Pedro A. Tresgallo <P3dro115@gmail.com>
2018-09-26 10:53:58 -0500

    Organized Solution

---
+++ pt-GAP-Week1/Chapter2/pt-MultiPoint/MultiPoint.js
@@ -0,0 +1,105 @@
+﻿// MultiPoint.js
+// Vertex shader program
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    '  gl_Position = a_Position;\n' +
+    '  gl_PointSize = 10.0;\n' +
+    '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    '  gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);\n' + // Set the color
+    '}\n';
+
+function main() {
+    // Retrieve <canvas> element
+    var canvas = document.getElementById('webgl');
+
+    // Get the rendering ontext for WebGL
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    // Get the positions of vertices
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    // Set the color for clear canvas
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Draw three points
+    gl.drawArrays(gl.points, 0, n); // n is 3
+}
+
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
+    var n = 3; // The number of vertices
+
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object ');
+        return -1;
+    }
+
+    // Bind the buffer object to the target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // Write date into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+    // Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
+
+/*
+var g_points = []; // The array for a mouse click
+function click(ev, gl, canvas, a_Position) {
+    var x = ev.clientX; // x coordinate of a mouse pointer
+    var y = ev.clientY; // y coordinate of a mouse pointer
+    var rect = ev.target.getBoundingClientRect();
+
+    x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2);
+    y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+    // Store the coordinates to g_Points array
+    g_points.push(x); g_points.push(y);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var len = g_points.length;
+    for (var i = 0; i < len; i += 2) {
+        // Pass the position of a point to a_Position variable
+        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+
+        // Draw a point
+        gl.drawArrays(gl.g_points, 0, 1);
+    }
+}*/
---
+++ pt-GAP-Week1/Chapter3/pt-RotatingTriangle/RotatingTriangle.js
@@ -0,0 +1,94 @@
+﻿// Rotating Triangle.js
+// Vertex Shader Program
+
+    // x' = x cos b - y sin b
+    // y' = x sin b + y cos b
+    // z' = z
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'uniform float u_CosB, u_SinB;\n' +
+    'void main() {\n' +
+    '  gl_Position.x = a_Position.x * u_CosB - a_Position.y * u_SinB;\n' +
+    '  gl_Position.y = a_Position.x * u_SinB + a_Position.y * u_CosB; \n' +
+    '  gl_Position.z - a_Position.z;\n' +
+    '  gl_Position.w = 1.0;\n' +
+    '}\n';
+
+// Fragment Shader
+var FSHADER_SOURCE =
+    'void main(){\n' +                                // creating the function for the Fragment Shader
+    '   gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' + // Setting the Fragment Shader color to yellow with alpha of 1
+    '}\n';  
+
+// Rotating angle
+var ANGLE = 90.0;
+
+function main() {
+    var canvas = document.getElementById('webgl');    // Grabbing the canvas element from the HTML file
+
+    var gl = getWebGLContext(canvas);                 // Rendering the canvas
+    if (!gl) {                                        // error log for canvas initialization
+        console.log('unable to get rendering context for web gl');  // the error message
+        return;                                                     // exits the program
+    }                                                               // Curly brace
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {         // Initializing shaders with an inline function
+        console.log('unable to load shaders');                      // Error log message
+        return;                                                     // exits the program
+    }                                                               // Curly brace
+
+    // Set the positions of vertices
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('failed to set the position');
+        return;
+    }
+
+    // Pass the data required to rotate
+
+    var radian = Math.PI * ANGLE / 180.0; // Convert to radian
+    var cosB = Math.cos(radian);
+    var sinB = Math.sin(radian);
+
+    var u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
+    var u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
+
+    gl.uniform1f(u_CosB, cosB);
+    gl.uniform1f(u_SinB, sinB);
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+} // main()
+
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
+
+    var n = 3;
+
+    var vertexBuffer = gl.createBuffer();                           // Creating buffer object + error message
+    if (!vertexBuffer) {
+        console.log('unable to create buffer object');
+        return;
+
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);       // Passing the vertices to the buffer object
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');// Getting the position of the triangle
+    if (a_Position < 0) {
+        console.log('failed to get source location');               // Error Message
+        return;
+    }
+
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);   // Drawing the triangle
+
+    gl.enableVertexAttribArray(a_Position);
+    return n;
+}
+
